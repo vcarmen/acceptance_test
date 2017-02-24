07f42fb4d1f9a5635b298ae7992add02260beda2
Carmen Villcazani <carmen.villcazani@jalasoft.com>
2017-02-23 20:54:47 -0400

    adding acceptance tests

---
+++ test/assert/assertReport.js
@@ -0,0 +1,34 @@
+'use strict';
+var fs = require('fs');
+var chai = require('chai');
+var expect = chai.expect;
+var config = require('../config/config.json');
+
+module.exports = {
+
+    assert: function(htmlFile) {
+        var jsonFile = htmlFile + '.json';
+
+        function assertJsonContents() {
+            var jsonOutput = require('../../' + jsonFile);
+            var jsonOutputStringify = JSON.stringify(jsonOutput);
+
+            //verify number of scenarios
+            var numberOfScenarios = 0;
+            jsonOutput.forEach(function(feature) {
+                numberOfScenarios += feature.elements.length;
+            });
+
+            expect(numberOfScenarios).to.be.equal(config.scenario.totalScenarios, 'Scenarios are missing in the report');
+            
+            //verify test data is attached to the report
+            expect(jsonOutputStringify).to.contain('mime_type":"text/plain"', 'test data was not attached to report');
+
+            //verify data-table is attached to the report
+            expect(jsonOutputStringify).to.contain('rows', 'data-table rows was not attached to report');
+            expect(jsonOutputStringify).to.contain('cells', 'data-table rows was not attached to report');
+        }
+        
+        return assertJsonContents();
+    }
+};
